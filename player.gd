215242f29408d3f7f4cf52adc7a31cbb8e6a0f14
Zamaroht <laurito200@gmail.com>
2015-05-24 21:43:41 -0300

    .

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+func _ready():
+	pass
+
+func _integrate_forces(s):
+	var lv = s.get_linear_velocity()
+	lv.y += 3
+	s.set_linear_velocity(lv)
+
+	var ccount = s.get_contact_count()
+	for i in range(ccount):
+		if s.get_contact_collider_object(i).get_type() == "TileMap":
+			var metadata = s.get_contact_collider_shape_metadata(i)
+			print(metadata)
